fbbdf441fc490158e90c8775ace9cc6ef853e0b9
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-06-30 11:43:50 +0800

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from '../Notification/Notification.module.css';
 
+export const Notification = ({message}) => (
+    <p className={s.notofication_text}>
+        {message}
+    </p>
+)
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired
+}
