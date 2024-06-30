f3ab97c254de40bbe213dc0ee1e2b07d4f850404
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-06-30 11:40:03 +0800

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from '../FeedbackOptions/FeedbackOptions.module.css';
 
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div className={s.feedback_container}>
+    {Object.keys(options).map(option => (
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={onLeaveFeedback}
+        className={s.feedback_btn}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
