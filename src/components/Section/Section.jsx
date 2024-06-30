aea79ee1928bd1b1268dcf513952863156700ef2
Ruel <134071413+Reuelle@users.noreply.github.com>
2024-06-30 11:46:45 +0800

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from '../Section/Section.module.css';
 
+export const Section = ({title, children}) => (
+    <section className={s.feedback}>
+        <h2 className={s.feedback_title}> {title}</h2>
+        {children}
+    </section>
+);
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+};
